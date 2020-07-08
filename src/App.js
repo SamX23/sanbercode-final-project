6ec07dfd9aab92c22149e2585a69313ce0d0361c
Sami Kalammallah <skalexsong@gmail.com>
2020-07-08 20:11:01 +0700

    update update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Header(){
+  return(
+    <header>
+      <h1>This is Header</h1>
+    </header>
+  )
+}
+
+function Main(){
+  return(
+    <main>
+      <h3>This is Main</h3>
+    </main>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>
+      <p>This is Header</p>
+    </footer>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
